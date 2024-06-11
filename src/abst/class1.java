2007672f0faacbc66d5603758cde9f0f4ebf77bf
syss <syss@sys>
2024-06-11 17:38:26 +0530

    first commit

---
+++ src/abst/class1.java
@@ -0,0 +1,31 @@
+package abst;
+
+public  abstract class class1 {
+
+	
+	void student()
+	
+	{
+		
+		System.out.println("i am non abstract");
+		
+	}
+	
+	// abstract class
+	
+	
+	abstract void student1 ();
+
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	
+
+	}
+
+}
---
+++ src/abst/class2.java
@@ -0,0 +1,30 @@
+package abst;
+
+public class class2 extends class1 {
+
+	
+	void student1()
+	{
+		
+		
+	}
+	
+	
+	void school()
+	{
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		class2 obj= new class2();
+		obj.student();
+		obj.student1();
+		
+
+	}
+
+}
